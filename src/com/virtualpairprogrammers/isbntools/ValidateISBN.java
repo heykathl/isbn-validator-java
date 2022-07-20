0241e3adece3f1772fa7c3341dabbad29ad3e7d8
Kathleen Ly <heykathl@users.noreply.github.com>
2022-07-20 16:30:41 +0100

    Extracted into two methods and creation of constants to remove magic numbers

--- src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
+++ src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -1,54 +1,68 @@
 package com.virtualpairprogrammers.isbntools;
 
 public class ValidateISBN {
+
+    public static final int LONG_ISBN = 13;
+    public static final int SHORT_ISBN = 10;
+    public static final int SHORT_ISBN_MULTIPLIER = 11;
+    public static final int LONG_ISBN_MULTIPLIER = 10;
+
     public boolean checkISBN(String isbn) {
 //        Checks whether isbn is 13 digits long
-        if (isbn.length() == 13) {
-            int total = 0;
-//          Multiply second digit by 1 and every other by 3
-            for (int i = 0; i < 13; i++) {
-                if (i % 2 == 0) {
-                    total += Character.getNumericValue(isbn.charAt(i));
-                } else {
-                    total += Character.getNumericValue(isbn.charAt(i)) * 3;
-                }
-            }
-
-            if (total % 10 == 0) {
-                return true;
-            } else {
-                return false;
-            }
+        if (isbn.length() == LONG_ISBN) {
+            return isValid13ISBN(isbn);
 
         } else {
 
 //          Checking whether isbn is 10 digits long
-            if (isbn.length() != 10) {
+            if (isbn.length() != SHORT_ISBN) {
 //            Alternative way to check if each character is a digit
 //            || !isbn.matches("\\d+")) {
                 throw new NumberFormatException("Error - invalid number");
             }
             ;
-            int total = 0;
-//        Checking that each digit is a number
-            for (int i = 0; i < 10; i++) {
-                if (!Character.isDigit(isbn.charAt(i))) {
-//              Checking that the last character is an X
-                    if (i == 9 && isbn.charAt(i) == 'X') {
-                        total += 10;
-                    } else {
-                        throw new NumberFormatException("Error");
-                    }
-                } else {
-                    total += Character.getNumericValue(isbn.charAt(i)) * (10 - i);
-                }
-            }
-//        Part of the isbn validation method to divide the total by 11
-                if (total % 11 == 0) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
+            return isValid10ISBN(isbn);
         }
     }
+
+    private boolean isValid10ISBN(String isbn) {
+        int total = 0;
+//        Checking that each digit is a number
+        for (int i = 0; i < SHORT_ISBN; i++) {
+            if (!Character.isDigit(isbn.charAt(i))) {
+//              Checking that the last character is an X
+                if (i == 9 && isbn.charAt(i) == 'X') {
+                    total += 10;
+                } else {
+                    throw new NumberFormatException("Error");
+                }
+            } else {
+                total += Character.getNumericValue(isbn.charAt(i)) * (SHORT_ISBN - i);
+            }
+        }
+//        Part of the isbn validation method to divide the total by 11
+        if (total % SHORT_ISBN_MULTIPLIER == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean isValid13ISBN(String isbn) {
+        int total = 0;
+//          Multiply second digit by 1 and every other by 3
+        for (int i = 0; i < LONG_ISBN; i++) {
+            if (i % 2 == 0) {
+                total += Character.getNumericValue(isbn.charAt(i));
+            } else {
+                total += Character.getNumericValue(isbn.charAt(i)) * 3;
+            }
+        }
+//        Part of the isbn validation method to divide the total by 10
+        if (total % LONG_ISBN_MULTIPLIER == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
