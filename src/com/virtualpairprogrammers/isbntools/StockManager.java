4e9517c54949b0ed9a3e7431b65b9fa8de40e426
Kathleen Ly <heykathl@users.noreply.github.com>
2022-07-27 11:42:22 +0100

    Included notes in getLocatorCode method

--- src/com/virtualpairprogrammers/isbntools/StockManager.java
+++ src/com/virtualpairprogrammers/isbntools/StockManager.java
@@ -11,8 +11,11 @@ public class StockManager {
     public String getLocatorCode(String isbn) {
         Book book = service.lookup(isbn);
         StringBuilder locator = new StringBuilder();
+        // Grab last four digits of isbn
         locator.append(isbn.substring(isbn.length() - 4));
+        // Grab first character of author
         locator.append(book.getAuthor().substring(0, 1));
+        // Grab number of words in title
         locator.append(book.getTitle().split(" ").length);
         return locator.toString();
     }
