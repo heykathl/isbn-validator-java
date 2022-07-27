8bced7e88c02a7f761bd66dfc67dcfc0d169127c
Kathleen Ly <heykathl@users.noreply.github.com>
2022-07-27 11:49:18 +0100

    Created mock of externalDataService to pass test

--- test/com/virtualpairprogrammers/isbntools/StockManagementTests.java
+++ test/com/virtualpairprogrammers/isbntools/StockManagementTests.java
@@ -9,8 +9,19 @@ public class StockManagementTests {
 
     @Test
     public void testGetCorrectLocatorCode() {
-        String isbn = "0140177396";
+
+        // Creating a test stub as there is dependency on an external service
+        ExternalISBNDataService testService = new ExternalISBNDataService() {
+            @Override
+            // Inject this stub into our class to ensure our business logic works
+            public Book lookup(String isbn) {
+                return new Book(isbn, "Of Mice And Men", "J. Steinbeck");
+            }
+        };
+
         StockManager stockManager = new StockManager();
+        stockManager.setService(testService);
+        String isbn = "0140177396";
         String locatorCode = stockManager.getLocatorCode(isbn);
         assertEquals("7396J4", locatorCode);
     }
